eeabb62e1870e715ad1cb361bc8f108c9566c8bb
Eddy Mena <85008352+devedm@users.noreply.github.com>
2023-03-03 16:01:13 -0600

    Changed folder names

--- Artifacts/windows-testArtifact/newscript.ps1
+++
@@ -1,7 +0,0 @@
-$url = "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u345-b01/OpenJDK8U-jdk_x64_windows_hotspot_8u345b01.msi"
-
-$destination = "C:\temp\OpenJDK8U-jdk_x64_windows_hotspot_8u345b01.msi"
-
-Invoke-WebRequest -Uri $url -OutFile $destination
-
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$destination`" ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome INSTALLDIR=`"c:\Program Files\Temurin`"" /quiet" -Wait
